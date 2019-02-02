afb04bc53e37e1dbd19c077359fd1d91b26e3dbe
wszqkzqk <wszqkzqk@gmail.com>
2019-02-02 22:34:53 +0800

    auto updated by update.sh

--- KDE-install.sh
+++ KDE-install.sh
@@ -10,3 +10,7 @@ echo '即将开始安装...'
 sudo dpkg -i *.deb
 echo '安装完成，正在自动安装依赖...'
 sudo apt install -f 
+echo '依赖安装完成正在配置环境'
+cp ./gsd-xsettings.sh $HOME/.config/autostart-scripts/gsd-xsettings.sh
+echo '执行完成'
+$HOME/.config/autostart-scripts/gsd-xsettings.sh
---
+++ gsd-xsettings.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/lib/gnome-settings-daemon/gsd-xsettings
