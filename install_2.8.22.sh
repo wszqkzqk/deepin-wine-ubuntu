7b33db1465fe6aa748610dfc6484dda460567d13
Stan Zhang <32720167+jz448@users.noreply.github.com>
2021-01-05 11:12:26 +0800

    Update install_2.8.22.sh
    Fixed broken links & sorted the installation order

--- install_2.8.22.sh
+++ install_2.8.22.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 mkdir ./deepintemp
 cd ./deepintemp
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-wine_2.18-22~rc0_all.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-wine32_2.18-22~rc0_i386.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-wine32-preloader_2.18-22~rc0_i386.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine-helper/deepin-wine-helper_1.2deepin8_i386.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine-plugin/deepin-wine-plugin_1.0deepin2_amd64.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine-plugin-virtual/deepin-wine-plugin-virtual_1.0deepin3_all.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine-uninstaller/deepin-wine-uninstaller_0.1deepin2_i386.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/u/udis86/udis86_1.72-2_i386.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-fonts-wine_2.18-22~rc0_all.deb
-wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-libwine_2.18-22~rc0_i386.deb
-wget https://mirrors.aliyun.com/deepin/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_amd64.deb
-wget https://mirrors.aliyun.com/deepin/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_i386.deb
+wget -O 1.1udis86_1.72-2_i386.deb https://community-packages.deepin.com/deepin/pool/non-free/u/udis86/udis86_1.72-2_i386.deb
+wget -O 1.2deepin-fonts-wine_2.18-22~rc0_all.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-fonts-wine_2.18-22~rc0_all.deb
+wget -O 2.1deepin-libwine_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-libwine_2.18-22~rc0_i386.deb
+wget -O 2.2deepin-libwine-dbg_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-libwine-dbg_2.18-22~rc0_i386.deb
+wget -O 2.3deepin-libwine-dev_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-libwine-dev_2.18-22~rc0_i386.deb
+wget -O 3.1deepin-wine32_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-wine32_2.18-22~rc0_i386.deb
+wget -O 3.2deepin-wine32-preloader_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-wine32-preloader_2.18-22~rc0_i386.deb
+wget -O 3.3deepin-wine32-tools_2.18-22~rc0_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-wine32-tools_2.18-22~rc0_i386.deb
+wget -O 4deepin-wine_2.18-22~rc0_all.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-wine_2.18-22~rc0_all.deb
+wget -O 5deepin-wine-binfmt_2.18-22~rc0_all.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine/deepin-wine-binfmt_2.18-22~rc0_all.deb
+wget -O 6.1deepin-wine-plugin_1.0deepin2_amd64.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine-plugin/deepin-wine-plugin_1.0deepin2_amd64.deb
+wget -O 6.2deepin-wine-plugin-virtual_1.0deepin3_all.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine-plugin-virtual/deepin-wine-plugin-virtual_1.0deepin3_all.deb
+wget -O 7deepin-wine-helper_1.2deepin8_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine-helper/deepin-wine-helper_1.2deepin8_i386.deb
+wget -O 8deepin-wine-uninstaller_0.1deepin2_i386.deb http://packages.deepin.com/deepin/pool/non-free/d/deepin-wine-uninstaller/deepin-wine-uninstaller_0.1deepin2_i386.deb
+wget http://ftp.debian.org/debian/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_amd64.deb
+wget http://ftp.debian.org/debian/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_i386.deb
 
 echo '准备添加32位支持'
 sudo dpkg --add-architecture i386
